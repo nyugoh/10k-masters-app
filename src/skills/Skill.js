82114542e15f9393590db1c47a955099ce82de59
nyugoh <nyugoh@gmail.com>
2018-05-23 03:24:28 +0300

    Includes components for the skill

--- src/skills/Skill.js
+++ src/skills/Skill.js
@@ -1,6 +1,10 @@
 import React, {Component} from "react";
 import { connect } from 'react-redux';
 import { Header, Icon, Label } from 'semantic-ui-react';
+import CountDown from '../Components/CountDown';
+import Progress from '../Components/Progress';
+import TimeControls from '../Components/TimeControls';
+import Activities from '../Components/Activities';
 
 class Skill extends Component {
 	constructor(props) {
@@ -14,10 +18,15 @@ class Skill extends Component {
 		if(skills.length > 0)
 			return (
 				<div>
-					<Header as={'h3'}>
-						<span style={{marginRight:20}}>{skill.subject}</span>
+					<Header as={'h1'}>
+						<span style={{marginRight:20, marginTop: 20}}>{skill.subject}</span>
 						<Label tag={true} style={{backgroundColor:skill.theme}}>{skill.hours} Hours</Label>
 					</Header>
+					<div className="ui divider"/>
+					<CountDown/>
+					<Progress/>
+					<TimeControls/>
+					<Activities/>
 				</div>
 			);
 		else
