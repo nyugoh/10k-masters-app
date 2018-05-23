71dda9ab1cec5bc7f822ce535254f5817c39d386
nyugoh <nyugoh@gmail.com>
2018-05-23 20:29:01 +0300

    Added button to respond to state changed

--- src/Components/TimeControls.js
+++ src/Components/TimeControls.js
@@ -1,14 +1,120 @@
 import React, {Component} from 'react';
-import { Segment, Button, Icon } from 'semantic-ui-react';
+import { Segment, Button, Icon, Message } from 'semantic-ui-react';
 
 class TimeControls extends Component {
+    startSession = () =>{
+        this.props.startSession();
+    };
+
+    pauseSession = () =>{
+        this.props.pauseSession();
+    };
+
+    resumeSession = () =>{
+        this.props.resumeSession();
+    };
+
+    endSession = () =>{
+        this.props.endSession();
+    };
+
     render() {
-        return (
-            <Segment className={'ui center aligned'} >
-                <Button size={'massive'} positive icon labelPosition={'left'}><Icon name={'play'}/> START</Button>
-                <Button size={'massive'} color={'teal'} icon labelPosition={'left'}><Icon name={'pause'}/> PAUSE</Button>
-            </Segment>
-        );
+        const { status } = this.props;
+        if( status === 'new') {
+            return (<div>
+                <Button
+                    size={'massive'}
+                    positive
+                    icon
+                    onClick={this.startSession}
+                    labelPosition={'left'}>
+                    <Icon name={'play'}/>
+                    START
+                </Button>
+                <Button
+                    disabled={true}
+                    size={'massive'}
+                    color={'teal'}
+                    icon
+                    onClick={this.pauseSession}
+                    labelPosition={'left'}>
+                    <Icon name={'pause'}/>
+                    PAUSE
+                </Button>
+            </div>);
+        } else if (status === 'in-progress') {
+            return (<div>
+                <Button
+                    size={'massive'}
+                    positive
+                    icon
+                    onClick={this.endSession}
+                    labelPosition={'left'}>
+                    <Icon name={'stop'}/>
+                    STOP
+                </Button>
+                <Button
+                    size={'massive'}
+                    color={'teal'}
+                    icon
+                    onClick={this.pauseSession}
+                    labelPosition={'left'}>
+                    <Icon name={'pause'}/>
+                    PAUSE
+                </Button>
+            </div>);
+        } else if (status === 'paused') {
+            return (<div>
+                <Button
+                    size={'massive'}
+                    positive
+                    icon
+                    onClick={this.endSession}
+                    labelPosition={'left'}>
+                    <Icon name={'stop'}/>
+                    STOP
+                </Button>
+                <Button
+                    size={'massive'}
+                    color={'teal'}
+                    icon
+                    onClick={this.resumeSession}
+                    labelPosition={'left'}>
+                    <Icon name={'play'}/>
+                    RESUME
+                </Button>
+            </div>);
+        } else if (status === 'complete') {
+            return ( <div>
+                <Button
+                    disabled={true}
+                    size={'massive'}
+                    positive
+                    icon
+                    onClick={this.endSession}
+                    labelPosition={'left'}>
+                    <Icon name={'stop'}/>
+                    STOP
+                </Button>
+                <Button
+                    disabled={true}
+                    size={'massive'}
+                    color={'teal'}
+                    icon
+                    onClick={this.pauseSession}
+                    labelPosition={'left'}>
+                    <Icon name={'pause'}/>
+                    PAUSE
+                </Button>
+            </div>);
+        } else {
+            return (
+              <Message>
+                  <Message.Header>Application error</Message.Header>
+                  <Message.Content>Service monkeys have been notified, they are working to restore services.</Message.Content>
+              </Message>
+            );
+        }
     }
 }
 
--- src/skills/Skill.js
+++ src/skills/Skill.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import { connect } from 'react-redux';
-import { Header, Icon, Label } from 'semantic-ui-react';
+import { Header, Icon, Label, Segment } from 'semantic-ui-react';
 import CountDown from '../Components/CountDown';
 import Progress from '../Components/Progress';
 import TimeControls from '../Components/TimeControls';
@@ -9,13 +9,27 @@ import Activities from '../Components/Activities';
 class Skill extends Component {
 	constructor(props) {
 		super(props);
-		this.skill = {};
+		this.state = {
+			skill: {},
+			session: {
+				status: 'new',
+				startTime: '',
+				endTime: ''
+			}
+		};
 	};
+
+	componentDidMount() {
+        if( this.props.skills.length > 0){
+            const skill = this.props.skills.filter( skill => skill._id === this.props.match.params.id)[0];
+            this.setState({ skill: skill });
+		}
+    };
 
 	render() {
 		const skills = this.props.skills;
-		const skill = skills.filter( skill => skill._id === this.props.match.params.id)[0];
-		if(skills.length > 0)
+		const { skill, session } = this.state;
+		if(skills.length > 0) {
 			return (
 				<div>
 					<Header as={'h1'}>
@@ -25,19 +39,43 @@ class Skill extends Component {
 					<div className="ui divider"/>
 					<CountDown/>
 					<Progress skill={skill}/><br/>
-					<TimeControls/>
+					<Segment className={'ui center aligned'} >
+						<TimeControls
+							startSession={this.startSession}
+							pauseSession={this.pauseSession}
+							resumeSession={this.resumeSession}
+							endSession={this.endSession}
+							status={session.status}/>
+						<Label pointing={'above'} color={'green'}>{session.status}</Label>
+					</Segment>
 					<div className="ui divider"/>
 					<br/><br/>
 					<Activities skill={skill}/>
-				</div>
-			);
-		else
+				</div>);
+		} else {
 			return (
 				<div>
 					<Header>Loading <Icon loading color={'teal'} name='spinner' /> </Header>
 				</div>
 			);
-	}
+		}
+	};
+
+    startSession = () =>{
+        this.setState({ session: { ...this.state.session, status: 'in-progress' }});
+    };
+
+    pauseSession = () =>{
+        this.setState({ session: { ...this.state.session, status: 'paused' }});
+    };
+
+    resumeSession = () =>{
+        this.setState({ session: { ...this.state.session, status: 'in-progress' }});
+    };
+
+    endSession = () =>{
+        this.setState({ session: { ...this.state.session, status: 'complete' }});
+    };
 }
 
 const mapStateToProps = state => ({
